68cdecfdf645e37ceb082f08eb89eb4531f46573
yukirikurisu <58366729+yukirikurisu@users.noreply.github.com>
2025-06-14 21:50:59 +0000

    revisar si no deshacer

--- gamewaifu-root/backend/db/connection.js
+++ gamewaifu-root/backend/db/connection.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const config = require('../config');
+const config = require('../config/config');
 
 let pool;
 
--- gamewaifu-root/backend/server.js
+++ gamewaifu-root/backend/server.js
@@ -14,7 +14,7 @@ const ResentService = require('./services/resentService');
 const characterRoutes = require('./routes/characters');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const config = require('./config');
+const config = require('./config/config');
 
 // Middleware
 app.use(cors({ 
